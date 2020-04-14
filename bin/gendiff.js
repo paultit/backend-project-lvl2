cffea5933f4420323bfa96bfe21b924121ba3691
pavlo <titarenko.pavel@gmail.com>
2020-04-14 14:24:29 +0300

    add-options-stage-2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,11 @@ import program from 'commander';
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig);
+    console.log(secondConfig);
+  });
 program.parse(process.argv);
