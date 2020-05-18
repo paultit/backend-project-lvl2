39b72aa684e89e460e914a9ad14bfbbb86fbe6bf
pavlo <titarenko.pavel@gmail.com>
2020-05-18 08:20:57 +0300

    refactoring-5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,16 +2,25 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const getFixuturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
-const readDataDiffFile = (diffFileName) => fs.readFileSync(getFixuturePath(diffFileName), 'utf-8').trim();
+const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
+const readDataDiffFile = (diffFileName) => fs.readFileSync(getFixturePath(diffFileName), 'utf-8').trim();
 
 describe('', () => {
-  test.each([[getFixuturePath('before-nested.json'), getFixuturePath('after-nested.json')],
-    [getFixuturePath('before-nested.yaml'), getFixuturePath('after-nested.yaml')],
-    [getFixuturePath('before-nested.ini'), getFixuturePath('after-nested.ini')],
-  ])('Compare files', (dataBefore, dataAfter) => {
-    expect(genDiff(dataBefore, dataAfter)).toEqual(readDataDiffFile('result-nested.txt'));
-    expect(genDiff(dataBefore, dataAfter, 'plain')).toEqual(readDataDiffFile('result-nested-plain.txt'));
-    expect(genDiff(dataBefore, dataAfter, 'json')).toEqual(readDataDiffFile('result-nested-json.txt'));
+  test.each([
+    ['json', 'plain'],
+    ['yaml', 'plain'],
+    ['ini', 'plain'],
+    ['json', 'json'],
+    ['yaml', 'json'],
+    ['ini', 'json'],
+    ['json', 'diff'],
+    ['yaml', 'diff'],
+    ['ini', 'diff'],
+  ])('Compare files', (extension, format) => {
+    const expected = readDataDiffFile(`result-nested-${format}.txt`);
+    const dataBefore = getFixturePath(`before-nested.${extension}`);
+    const dataAfter = getFixturePath(`after-nested.${extension}`);
+    const actual = genDiff(dataBefore, dataAfter, format);
+    expect(actual).toEqual(expected);
   });
 });
